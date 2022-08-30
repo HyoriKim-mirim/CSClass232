4e3226bd2aab23013948ed0019f2b654755a49c2
Hyori Kim <s2010@e-mirim.hs.kr>
2022-08-30 23:14:56 +0900

    인터페이스 소개 - IDisposable

--- CSClass232/Program.cs
+++ CSClass232/Program.cs
@@ -1,7 +1,4 @@
-﻿using DocumentFormat.OpenXml.Drawing;
-using DocumentFormat.OpenXml.Drawing.Diagrams;
-using Microsoft.Azure.Documents.Spatial;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -14,7 +11,7 @@ namespace CSClass232
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             Wanted<String> wantedString = new Wanted<string>("String");
             Wanted<int> wantedInt = new Wanted<int>(52273);
             Wanted<double> wantedDouble = new Wanted<double>(52.273);
@@ -71,16 +68,19 @@ namespace CSClass232
             Console.WriteLine("pointStructA: " + pointStructA.x + ", " + pointStructA.y);
             Console.WriteLine("pointStructB: " + pointStructB.x + ", " + pointStructB.y);
 
-            List<Product> list = new List<Product>() {
-                new Product(){Name="고구마", Price=1500},
-                new Product(){Name="사과", Price=2400},
-                new Product(){Name="바나나", Price=1000},
-                new Product(){Name="배", Price=3000}
-            };
-            list.Sort();
-            foreach (var item in list)
+            using (Dummy dummy = new Dummy())
             {
-                Console.WriteLine(item);
+                List<Product> list = new List<Product>() {
+                    new Product(){Name="고구마", Price=1500},
+                    new Product(){Name="사과", Price=2400},
+                    new Product(){Name="바나나", Price=1000},
+                    new Product(){Name="배", Price=3000}
+                };
+                list.Sort();
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
             }
         }
         static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
@@ -130,13 +130,21 @@ namespace CSClass232
             {
                 this.x = x;
                 this.y = y;
-                testA = s; 
+                testA = s;
                 testB = s;
             }
 
         }
 
-        
+        class Dummy : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose() 메서드를 호출했습니다.");
+            }
+        }
+
+
     }
 
 
